dde9005add85d7d217d5c6c6baccd8d9b7a37591
Cássio <cdrl@cin.ufpe.br>
2025-01-19 17:15:36 -0300

    feat: add event editing

--- app/services/event_service.py
+++ app/services/event_service.py
@@ -4,6 +4,9 @@ from app import db
 class EventAlreadyExistsException(Exception):
     pass
 
+class EventNotFoundException(Exception):
+    pass
+
 def createEvent(data):
     # checks for existing event name
     if Event.query.filter_by(name=data['name']).first():
@@ -21,6 +24,28 @@ def createEvent(data):
     db.session.commit()
 
     # converts event obj into dict for jsonify
+    eventDict = {"id": event.id, 
+                  "name": event.name, 
+                  "photographer": event.photographer, 
+                  "photographerLink": event.photographerLink, 
+                  "userId": event.userId, 
+                  "userName": event.userName}
+
+    return eventDict
+
+def editEvent(data):
+    # checks if event exists by the received id
+    event = Event.query.filter_by(id=data['id']).first()
+    if not event:
+        raise EventNotFoundException("Event not found.") # if no event is found with that id, raise exception
+    
+    # updates to new values
+    event.name = data.get('name', event.name)
+    event.photographer = data.get('photographer', event.photographer)
+    event.photographerLink = data.get('photographerLink', event.photographerLink)
+
+    db.session.commit()
+
     eventDict = {"id": event.id, 
                   "name": event.name, 
                   "photographer": event.photographer, 
