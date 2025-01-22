852ec032fdbef28c99ef67595d9e41ce1dfe1e5a
Fábio Papais <flfp@cin.ufpe.br>
2025-01-22 15:40:10 -0300

    fix: adds /find route into blueprints

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,8 @@
 from app.routes.auth import auth_bp
 from app.routes.event import event_bp
+from app.routes.find import find_bp
 
 def registerBlueprints(app):
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(event_bp, url_prefix='/event')
+    app.register_blueprint(find_bp, url_prefix='/find')
