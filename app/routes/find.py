a3f8367e7753fbeea9b7ea815b5fa0e8a2ac6d57
Cássio <cdrl@cin.ufpe.br>
2025-01-25 18:09:58 -0300

    feat: implement face verification using DeepFace

--- app/routes/find.py
+++ app/routes/find.py
@@ -2,6 +2,7 @@ import json
 from flask import Blueprint, request, jsonify
 from app.services.event_service import getEventData, EventNotFoundException
 from app.services.image_service import getImages
+from app.services.find_service import findImagesOnEvent
 
 find_bp = Blueprint('find', __name__)
 
@@ -13,9 +14,15 @@ def getEvent():
         event = getEventData(data)
         images_paths = getImages(event['id'])
 
-        # TODO: filter images_paths based on face verification and return images_paths
+        # get paths for each event image
+        event_image_paths = []
+        for image in images_paths:
+            event_image_paths.append(image['link'])
 
-        return jsonify(event), 200
+        # return matching images paths
+        matching_images = findImagesOnEvent(event_image_paths, files)
+
+        return jsonify(matching_images), 200
     except EventNotFoundException as e:
         return jsonify({"error": str(e)}), 404
     except ValueError as e:
--- app/services/find_service.py
+++ app/services/find_service.py
@@ -1,15 +1,13 @@
 from flask import current_app
 from werkzeug.utils import secure_filename
-import zipfile
+from deepface import DeepFace
 import os
 
-from app.models import Event, User
-from app import db
 
-
+# locates each image in the event folder and compares it to the uploaded image using face recognition
 def findImagesOnEvent(events_paths, file):
     # Ensure the folder for images exists
-    images_folder = os.path.join(current_app.config['IMAGES_FOLDER'], '/find')
+    images_folder = os.path.join(current_app.config['IMAGES_FOLDER'], 'find')
     os.makedirs(images_folder, exist_ok=True)
 
     if not (file.filename.endswith('.png') or file.filename.endswith('.jpg')):
@@ -20,6 +18,27 @@ def findImagesOnEvent(events_paths, file):
     image_path = os.path.join(images_folder, filename)
     file.save(image_path)
 
-    # TODO:
-    # The file path is at image_path
-    # use the file path to find the images in the events_paths
+    # list for matching images
+    matching_images = []
+
+    # go through each image path
+    for event_image_path in events_paths:
+
+        # TODO: fix this workaround, the event image path should be saved without the 'app' prefix by default
+        event_image_path = 'app' + event_image_path
+        
+        try:           
+            # use deepface to compare the images
+            # TODO: test different models and detector backends to find the best performance
+            result = DeepFace.verify(image_path, 
+                                     event_image_path, 
+                                     model_name="VGG-Face", 
+                                     detector_backend="opencv")
+                
+            if result["verified"]:
+                matching_images.append(event_image_path)  
+        # deepFace throws an exception if it can't find a face in the image, most common type of error
+        except Exception as e:
+            print(f"Error processing image {event_image_path}: {e}")
+
+    return matching_images
