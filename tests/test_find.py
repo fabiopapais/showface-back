6c0c68a7df277f6e838dfeb41e3e8db3cd26c7df
Cássio <cdrl@cin.ufpe.br>
2025-02-19 16:13:09 -0300

    tests: test_find for find route

---
+++ tests/test_find.py
@@ -0,0 +1,25 @@
+import pytest
+import json, os
+from tests.testutils import regNewUser, createNewEvent, delEventImages
+
+def testFindImages(client):
+
+    regNewUser(client)
+    createNewEvent(client)
+
+    with open("tests/files/selfie.png", "rb") as userSelfie:
+
+        eventIdJson = json.dumps({
+            "id": 1
+        })
+
+        file = (userSelfie, "selfie.png")
+
+        response = client.get(
+            "/find/",
+            data={"data": eventIdJson, "file": file},
+            content_type="multipart/form-data"
+        )
+    
+    assert response.status_code == 200
+    assert os.path.isdir("app/static/images/find")
