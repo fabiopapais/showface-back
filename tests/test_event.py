720e61cb6c49723792701e180b906ef55a96214a
Cássio <cdrl@cin.ufpe.br>
2025-02-19 18:32:47 -0300

    tests: added full test coverage for event routes

--- tests/test_event.py
+++ tests/test_event.py
@@ -1,14 +1,11 @@
 import pytest
-import json
-import os, shutil
-import time
+import json, os, time
+from tests.testutils import regNewUser, createNewEvent, delEventImages
 
 def testNewEvent(client):
 
-    client.post(
-        "/auth/register",
-        json={"name": "testuser", "email": "test@test.com", "password": "securepswd"},
-    )
+    # register a new user in db
+    regNewUser(client)
 
     with open("tests/files/images.zip", "rb") as imageZip:
 
@@ -37,5 +34,52 @@ def testNewEvent(client):
     assert os.path.isdir("app/static/images/1")
 
     # removing the event images folder created:
-    if os.path.isdir("app/static/images/"):
-        shutil.rmtree("app/static/images/")
+    delEventImages()
+
+def testEditEvent(client):
+
+    # register a new user in db
+    regNewUser(client)
+
+    # create a new event in db
+    createNewEvent(client)
+
+    # edit the event
+    response = client.put(
+        "/event/edit",
+        json={
+            "id": 1,
+            "name": "Edited Event",
+            "photographer": "Edited User",
+            "photographerLink": "editeduser.com",
+            "userId": 1,
+            "userName": "testuser"
+            }
+    )
+
+    #assert the event was edited
+    assert response.status_code == 200
+    info = response.get_json()
+    assert info["name"] == "Edited Event"
+
+    # removing the event images folder created:
+    delEventImages()
+
+def testGetEvent(client):
+
+    # register a new user in db
+    regNewUser(client)
+
+    # create a new event in db
+    createNewEvent(client)
+
+    # get the event
+    response = client.get(
+        "/event/1",
+    )
+
+    #assert the event was retrieved
+    assert response.status_code == 200
+
+    # removing the event images folder created:
+    delEventImages()
---
+++ tests/testutils.py
@@ -0,0 +1,47 @@
+import json
+import time
+import os, shutil
+
+#USEFUL FUNCTIONS FOR TESTING WILL BE HERE AND IMPORTED TO OTHER FILES FROM HERE
+
+def regNewUser(client):
+    client.post(
+        "/auth/register",
+        json={"name": "testuser", "email": "test@test.com", "password": "securepswd"},
+    )
+
+def createNewEvent(client):
+    client.post(
+        "/auth/register",
+        json={"name": "testuser", "email": "test@test.com", "password": "securepswd"},
+    )
+
+    with open("tests/files/images.zip", "rb") as imageZip:
+
+        eventDataJson = json.dumps({
+            "name": "Example Event",
+            "photographer": "User",
+            "photographerLink": "user.com",
+            "userId": 1,
+            "userName": "testuser"
+        })
+
+        file = (imageZip, "images.zip")
+
+        response = client.post(
+            "/event/new",
+            data={"data": eventDataJson, "file": file},
+            content_type="multipart/form-data"
+        )
+
+    # delay so the background task preGenerateRepresentations can finish
+    time.sleep(10)
+
+    assert response.status_code == 201
+    info = response.get_json()
+    assert "id" in info
+    assert os.path.isdir("app/static/images/1")
+
+def delEventImages():
+    if os.path.isdir("app/static/images/"):
+        shutil.rmtree("app/static/images/")
